964857209440791dafb2ae5929d5ee2817c796c8
Nicolas Oudard <noudard@weborama.com>
2012-03-12 15:33:16 +0100

    new release 0.03

--- lib/Dancer/Plugin/Fake/Response.pm
+++ lib/Dancer/Plugin/Fake/Response.pm
@@ -13,11 +13,11 @@ Dancer::Plugin::Fake::Response - The great new Dancer::Plugin::Fake::Response!
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
